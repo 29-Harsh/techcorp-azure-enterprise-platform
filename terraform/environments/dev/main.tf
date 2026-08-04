5719cd0c4e85693b0fa328ef826d577e65ca3f7f
Harsh Sharma <52374089+29-Harsh@users.noreply.github.com>
2026-08-04 15:39:23 +0530

    added vnet and fix intendation

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,4 +1,10 @@
 module "resource_group" {
-  source = "../../modules/resource_group"
+  source  = "../../modules/resource_group"
   rgChild = var.rg
+}
+
+module "virtual_network" {
+  depends_on = [module.resource_group]
+  source     = "../../modules/virtual_network"
+  vnetChild  = var.vnets
 }
