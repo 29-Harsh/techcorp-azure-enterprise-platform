71da08fcabb15ba7c2c646cd23de3377f7ad4421
Harsh Sharma <52374089+29-Harsh@users.noreply.github.com>
2026-08-04 15:40:45 +0530

    added vnet and fix intendation

--- terraform/modules/resource_group/main.tf
+++ terraform/modules/resource_group/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rghs" {
-    for_each = var.rgChild
-    name = each.key
-    location = each.value
+  for_each = var.rgChild
+  name     = each.key
+  location = each.value
 }
